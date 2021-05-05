775a30c9dff191b9fdeb8c7ce1dac346abb9e286
Thai Nguyen <hongthaiktv@gmail.com>
2021-05-05 10:37:17 +0700

    update by sync.sh

--- sync.sh
+++ sync.sh
@@ -5,7 +5,10 @@ if [ -z "$msg" ]
 then
     msg="update by sync.sh"
 fi
+echo
 echo "Starting update with commit message: ""$msg"
+echo "_____________________________________________________________________"
+echo
 git add .
 git commit -m "$msg"
 sudo -g net7575 git pull
